a62f06fecbecfdb8d6f2ffb86e2d107137b3d505
ivanovandrey78 <jaydenpull1@gmail.com>
2026-01-15 16:20:29 +0300

    test(main): add tests to main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
-#include <cstdint>
+#include "logger.h"
 
 int main() {
-  return 0;
+    Logger::SetLevel(LogLevel::DEBUG);
+
+    Logger::Debug("Application started");
+    Logger::Info("Initializing modules...");
+    Logger::Warn("Config file not found, using defaults");
+    Logger::Error("Failed to connect to database");
+
+    Logger::SetFileOutput("app.log");
+
+    Logger::Info("Switched to file logging");
+    Logger::Debug("Processing data...");
+    Logger::Error("Critical error occurred");
+
+    Logger::SetConsoleOutput();
+    Logger::Info("Back to console output");
+
+    Logger::SetLevel(LogLevel::WARN);
+    Logger::Debug("This won't be shown");
+    Logger::Info("This won't be shown either");
+    Logger::Warn("This will be shown");
+    Logger::Error("This will be shown too");
+
+    return 0;
 }
